e16a5772c81e7f1e7c49eef5cd36544e625abf83
Savitskiy <sovick90@bk.ru>
2022-11-09 19:31:38 +0300

    Сверстал AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 import './Main.css'
 
 
@@ -7,6 +8,7 @@ function Main() {
   return (
     <main>
       <Promo />
+      <AboutProject />
     </main>
 
   )

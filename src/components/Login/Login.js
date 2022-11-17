bb932df11927bc4595b8509068c1a91ce359e4ad
Savitskiy <sovick90@bk.ru>
2022-11-17 20:25:13 +0300

    Сверстал login and register  под  адаптив

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,51 +1,49 @@
 import { Link } from "react-router-dom";
 import "./Login.css";
-import headerLogo from "./../../image/logo.svg";
+import formLogo from "./../../image/logo.svg";
 
 function Login() {
   return (
-    <div className="login__container">
-      <form className="login__form">
-        <img className="header__logo" src={headerLogo} alt="Логотип" />
-        <h2 className="login__title">Рады видеть!</h2>
-        <label className="form__input-label">Email</label>
+    <section className="login">
+      <form className="login__form" >
+        <img className="form__logo" src={formLogo} alt="Логотип" />
+        <h2 className="form__title">Рады видеть!</h2>
+        <label className="form__input-label">E-mail</label>
         <input
           type="Email"
-          className="login__input login__input_green"
+          className="form__input"
           id="email-profile"
           name="email"
           minLength="2"
           maxLength="40"
-          // value={email}
-          // onChange={({ target }) => setEmail(target.value)}
           autoComplete="on"
           required
         />
-        <label className="form__input-label">Пароль</label>
+        <span class="form__error" id="email-error"></span>
 
+        <label className="form__input-label">Пароль</label>
         <input
           type="password"
-          className="login__input login__input_password_active"
+          className="form__input form__input_password_active"
           id="password-profile"
           name="password"
           autoComplete="on"
           minLength="2"
           maxLength="200"
           required
-        // value={password}
-        // onChange={({ target }) => setPassword(target.value)}
         />
-        <button type="submit" className="login__submit login__submit_margin-login">
-          Зарегистрироваться
+        <span class="form__error" id="password-error"></span>
+        <button type="submit" className="form__submit form__submit_margin-login">
+          Войти
         </button>
-        <p className="login__link-text">
-          Уже зарегистрированы?
-          <Link to="signin" className="login__link  ">
-            Войти
+        <p className="form__link-text">
+          Ещё не зарегистрированы?
+          <Link to="signin" className="form__link  ">
+            Регистрация
           </Link>
         </p>
       </form>
-    </div>
+    </section>
   );
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -4,56 +4,54 @@ import headerLogo from "./../../image/logo.svg";
 
 function Register() {
   return (
-    <div className="login__container">
-      <form className="login__form">
-        <img className="header__logo" src={headerLogo} alt="Логотип" />
-        <h2 className="login__title">Добро пожаловать!</h2>
+    <div className="register">
+      <form className="register__form">
+        <img className="form__logo" src={headerLogo} alt="Логотип" />
+        <h2 className="form__title">Добро пожаловать!</h2>
         <label className="form__input-label">Имя</label>
         <input
           type="text"
-          className="login__input popup__input_email_active"
+          className="form__input"
           id="name-profile"
           name="name"
           minLength="2"
           maxLength="40"
-          // value={email}
-          // onChange={({ target }) => setEmail(target.value)}
           autoComplete="on"
           required
         />
+        <span class="form__error-signup" id="name-error"></span>
+
         <label className="form__input-label">Email</label>
         <input
           type="Email"
-          className="login__input login__input_green"
+          className="form__input form__input_green"
           id="email-profile"
           name="email"
           minLength="2"
           maxLength="40"
-          // value={email}
-          // onChange={({ target }) => setEmail(target.value)}
           autoComplete="on"
           required
         />
-        <label className="form__input-label">Пароль</label>
+        <span class="form__error-signup" id="email-error"></span>
 
+        <label className="form__input-label">Пароль</label>
         <input
           type="password"
-          className="login__input login__input_password_active"
+          className="form__input form__input_password_active"
           id="password-profile"
           name="password"
           autoComplete="on"
           minLength="2"
           maxLength="200"
           required
-        // value={password}
-        // onChange={({ target }) => setPassword(target.value)}
         />
-        <button type="submit" className="login__submit">
+        <span class="form__error-signup" id="password-error"></span>
+        <button type="submit" className="form__submit">
           Зарегистрироваться
         </button>
-        <p className="login__link-text">
+        <p className="form__link-signup-text">
           Уже зарегистрированы?
-          <Link to="signin" className="login__link  ">
+          <Link to="signin" className="form__link  ">
             Войти
           </Link>
         </p>

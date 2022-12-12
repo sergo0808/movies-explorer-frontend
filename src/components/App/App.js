3c8f0e866bc868cafb366dfa2cff425baa90596d
Savitskiy <sovick90@bk.ru>
2022-12-13 02:03:49 +0300

    fix bags render

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -40,6 +40,7 @@ function App() {
   const [amountRenderedCards, setAmountRenderedCards] = useState(selectAmountDefaultCards());
   const [savedMovies, setSavedMovies] = useState([]);
   const [foundFromSavedMovies, setFoundFromSavedMovies] = useState([]);
+  const foundCardsLocalStorage = JSON.parse(localStorage.getItem("foundCards"));
 
   const signOut = () => {
     localStorage.clear();
@@ -183,7 +184,7 @@ function App() {
 
   const getMovies = ({ textSearch, isChecked }) => {
     const filterCards = filtercards(cards, textSearch, isChecked);
-    if (cards.length === 0) {
+    if (cards.length === 0 && textSearch) {
       setIsLoading(true);
       moviesApi
         .getMovies()
